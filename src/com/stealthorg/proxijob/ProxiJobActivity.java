7fbb3fb5dd2dd93355394aec54eea8bb64207958
Bijan Amirzada <bamirzada@yahoo.com>
2012-10-27 02:37:05 -0700

    Initial Drop of Code

---
+++ src/com/stealthorg/proxijob/ProxiJobActivity.java
@@ -0,0 +1,13 @@
+package com.stealthorg.proxijob;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProxiJobActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
